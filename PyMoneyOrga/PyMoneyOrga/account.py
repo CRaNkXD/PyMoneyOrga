1f3f1f9aac7d7a7c6c1db4fee089f261b7e96e20
CRaNkXD <crank.xd@outlook.com>
2020-06-07 00:33:29 +0200

    fixed accoun properties acc_name and cash_in_euro

--- PyMoneyOrga/PyMoneyOrga/account.py
+++ PyMoneyOrga/PyMoneyOrga/account.py
@@ -3,21 +3,21 @@ class Account(object):
     """Saves the amount of cash in an account"""
 
     def __init__(self, acc_name, cash_in_euro):
-        self.acc_name = acc_name
-        self.cash_in_euro = cash_in_euro
+        self._acc_name = acc_name
+        self._cash_in_euro = cash_in_euro
         
     
     def add_cash_in_euro(self, amount_in_euro):
-        self.cash_in_euro += amount_in_euro
+        self._cash_in_euro += amount_in_euro
 
     def add_expenses_in_euro(self, amount_in_euro):
-        self.cash_in_euro -= amount_in_euro
-        print(self.cash_in_euro)
+        self._cash_in_euro -= amount_in_euro
+        print(self._cash_in_euro)
     
     @property
     def cash_in_euro(self):
-        return self.cash_in_euro
+        return self._cash_in_euro
     
     @property
     def acc_name(self):
-        return self.acc_name
+        return self._acc_name
