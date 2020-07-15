e722ec58f99f11139ce93066e92796c8db3f9145
CRaNkXD <crank.xd@outlook.com>
2020-07-15 15:01:58 +0200

    added comments

--- PyMoneyOrga/PyMoneyOrga/service_layer/services_account.py
+++ PyMoneyOrga/PyMoneyOrga/service_layer/services_account.py
@@ -2,6 +2,9 @@ from ..database.database_interface import DatabaseInterface
 
 
 def add_income(database: DatabaseInterface, acc_name, amount, description=None):
+    """
+    adds an income to the specified account in the database
+    """
     session = database.get_session()
     acc = database.get_acc(session, acc_name)
     acc.add_income(amount, description)
@@ -10,6 +13,9 @@ def add_income(database: DatabaseInterface, acc_name, amount, description=None):
 
 
 def add_expense(database: DatabaseInterface, acc_name, amount, description=None):
+    """
+    adds an expense to the specified account in the database
+    """
     session = database.get_session()
     acc = database.get_acc(session, acc_name)
     acc.add_expense(amount, description)
@@ -18,6 +24,9 @@ def add_expense(database: DatabaseInterface, acc_name, amount, description=None)
 
 
 def add_acc(database: DatabaseInterface, acc_name, balance):
+    """
+    adds a new account to the database
+    """
     session = database.get_session()
     database.add_acc(session, acc_name, balance)
     database.commit(session)
@@ -25,6 +34,9 @@ def add_acc(database: DatabaseInterface, acc_name, balance):
 
 
 def delete_acc(database: DatabaseInterface, acc_name):
+    """
+    adds the specified account from the database
+    """
     session = database.get_session()
     database.delete_acc(session, acc_name)
     database.commit(session)
@@ -32,6 +44,10 @@ def delete_acc(database: DatabaseInterface, acc_name):
 
 
 def get_all_acc(database: DatabaseInterface):
+    """
+    returns all accounts which are saved in the database
+    if not existent returns an empty list []
+    """
     session = database.get_session()
     accs = database.get_all_acc(session)
     database.close(session)
@@ -39,6 +55,10 @@ def get_all_acc(database: DatabaseInterface):
 
 
 def get_acc(database: DatabaseInterface, acc_name):
+    """
+    returns the specified account from the database
+    if not existent returns None
+    """
     session = database.get_session()
     acc = database.get_acc(session, acc_name)
     database.close(session)
@@ -46,6 +66,10 @@ def get_acc(database: DatabaseInterface, acc_name):
 
 
 def get_transactions(database: DatabaseInterface, acc_name):
+    """
+    returns all transactions from the specified account
+    if not existent returns an empty list []
+    """
     session = database.get_session()
     acc = database.get_acc(session, acc_name)
     if acc is None:
