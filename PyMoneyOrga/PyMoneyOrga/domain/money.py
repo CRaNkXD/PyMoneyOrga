4aaf3349c1ad8524c39cb72c155a1e23ccca9630
CRaNkXD <crank.xd@outlook.com>
2020-07-22 17:41:10 +0200

    added operator overloading for self and int

--- PyMoneyOrga/PyMoneyOrga/domain/money.py
+++ PyMoneyOrga/PyMoneyOrga/domain/money.py
@@ -15,11 +15,96 @@ class Money(object):
     def __str__(self):
         return f"{self._amount} {self._currency}"
 
-    def __add__(self, money):
-        if self._currency != money.currency:
-            print(f"Cannot add money when the currency does not match ({self._currency} != {money.currency}")
-        amount = self._amount + money.amount
-        return Money(amount, self._currency) 
+    def __int__(self):
+        return self._amount
+
+    def __add__(self, other):
+        if isinstance(other, self.__class__):
+            if self._currency != other.currency:
+                print(f"Cannot add money when the currency does not match ({self._currency} != {money.currency}")
+            amount = self._amount + other.amount
+            return Money(amount, self._currency)
+        elif isinstance(other, int):
+            amount = self._amount + other
+            return Money(amount, self._currency)
+        else:
+            raise TypeError(f"unsupported operand type(s) for +: '{self.__class__}' and '{type(other)}'")
+
+    def __iadd__(self, other):
+        if isinstance(other, self.__class__):
+            if self._currency != other.currency:
+                print(f"Cannot add money when the currency does not match ({self._currency} != {money.currency}")
+            self._amount += other.amount
+        elif isinstance(other, int):
+            self._amount += other
+        else:
+            raise TypeError(f"unsupported operand type(s) for +: '{self.__class__}' and '{type(other)}'")
+
+    def __sub__(self, other):
+        if isinstance(other, self.__class__):
+            if self._currency != other.currency:
+                print(f"Cannot substract money when the currency does not match ({self._currency} != {money.currency}")
+            amount = self._amount - other.amount
+            return Money(amount, self._currency)
+        elif isinstance(other, int):
+            amount = self._amount - other
+            return Money(amount, self._currency)
+        else:
+            raise TypeError(f"unsupported operand type(s) for -: '{self.__class__}' and '{type(other)}'")
+
+    def __isub__(self, money):
+        if isinstance(other, self.__class__):
+            if self._currency != money.currency:
+                print(f"Cannot substract money when the currency does not match ({self._currency} != {money.currency}")
+            self._amount -= money.amount
+        elif isinstance(other, int):
+            self._amount -= other
+        else:
+            raise TypeError(f"unsupported operand type(s) for -: '{self.__class__}' and '{type(other)}'")
+
+    def __mul__(self, other):
+        if isinstance(other, self.__class__):
+            if self._currency != other.currency:
+                print(f"Cannot multiply money when the currency does not match ({self._currency} != {money.currency}")
+            amount = self._amount * other.amount
+            return Money(amount, self._currency)
+        elif isinstance(other, int):
+            amount = self._amount * other
+            return Money(amount, self._currency)
+        else:
+            raise TypeError(f"unsupported operand type(s) for *: '{self.__class__}' and '{type(other)}'")
+
+    def __imul__(self, other):
+        if isinstance(other, self.__class__):
+            if self._currency != other.currency:
+                print(f"Cannot multiply money when the currency does not match ({self._currency} != {money.currency}")
+            self._amount *= other.amount
+        elif isinstance(other, int):
+            self._amount *= other
+        else:
+            raise TypeError(f"unsupported operand type(s) for *: '{self.__class__}' and '{type(other)}'")
+
+    def __truediv__(self, other):
+        if isinstance(other, self.__class__):
+            if self._currency != other.currency:
+                print(f"Cannot divide money when the currency does not match ({self._currency} != {money.currency}")
+            amount = self._amount // other.amount
+            return Money(amount, self._currency)
+        elif isinstance(other, int):
+            amount = self._amount // other
+            return Money(amount, self._currency)
+        else:
+            raise TypeError(f"unsupported operand type(s) for /: '{self.__class__}' and '{type(other)}'")
+
+    def __idiv__(self, other):
+        if isinstance(other, self.__class__):
+            if self._currency != other.currency:
+                print(f"Cannot divide money when the currency does not match ({self._currency} != {money.currency}")
+            self._amount //= other.amount
+        elif isinstance(other, int):
+            self._amount //= other
+        else:
+            raise TypeError(f"unsupported operand type(s) for /: '{self.__class__}' and '{type(other)}'")
 
     @property
     def amount(self):
@@ -30,8 +115,8 @@ class Money(object):
         return self._currency
 
     @classmethod
-    def convert_to(money: Money, new_currency):
-        if new_currency not in Money._VALID_CURRENCIES:
+    def convert_to(cls, money: 'Money', new_currency):
+        if new_currency not in cls._VALID_CURRENCIES:
             print(f"The currency {new_currency} is not valid or not implemented!")
-        euro = money.amount / Money._VALID_CURRENCIES[money.currency]
-        return Money(euro * Money._VALID_CURRENCIES[new_currency], new_currency)
+        euro = money.amount // cls._VALID_CURRENCIES[money.currency]
+        return cls(euro * cls._VALID_CURRENCIES[new_currency], new_currency)
