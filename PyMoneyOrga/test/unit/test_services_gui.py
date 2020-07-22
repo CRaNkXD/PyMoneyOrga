c9e56f6c0de1338349027718cee824c9a8e63772
CRaNkXD <crank.xd@outlook.com>
2020-07-22 17:40:18 +0200

    deleted obsolete TC

--- PyMoneyOrga/test/unit/test_services_gui.py
+++ PyMoneyOrga/test/unit/test_services_gui.py
@@ -3,22 +3,9 @@ from PyMoneyOrga.service_layer import services_gui
 from PyMoneyOrga.service_layer import services_account
 from PyMoneyOrga.database.database_sqlite import DatabaseSqlite
 
-class TestServicesAccount(unittest.TestCase):
-    def setUp(self):
-        self.database = DatabaseSqlite(url="sqlite:///pymoneyorga.sqlite.test")
-        self.acc_name_1 = "User1"
-        self.acc_name_2 = "User2"
-        return super().setUp()
-
-    def test_get_all_acc_names(self):
-        services_account.add_acc(self.database, self.acc_name_1, 100)
-        services_account.add_acc(self.database, self.acc_name_2, 200)
-        acc_names = services_gui.get_all_acc_names(self.database)
-        self.assertEqual(acc_names, [self.acc_name_1, self.acc_name_2])
-
-    def tearDown(self):
-        self.database._clear_all_tables()
-        return super().tearDown()
+class TestServicesGui(unittest.TestCase):
+    def test_A(self):
+        self.fail("Not implemented")
 
 if __name__ == '__main__':
     unittest.main()
