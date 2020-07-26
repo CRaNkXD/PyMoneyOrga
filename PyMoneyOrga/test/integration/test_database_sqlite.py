1dc9985e18ccc8ccbb17ffafd0c7411ff66b1e4b
CRaNkXD <crank.xd@outlook.com>
2020-07-26 18:10:28 +0200

    fixed new usage of context manager

--- PyMoneyOrga/test/integration/test_database_sqlite.py
+++ PyMoneyOrga/test/integration/test_database_sqlite.py
@@ -13,80 +13,34 @@ class TestDatabaseSqlite(unittest.TestCase):
         """
         tests if an account can be set by checking table columns
         """
-        session = self.database.get_session()
-        self.database.add_acc(session, self.acc_name_1, 100)
-        self.database.commit(session)
-        acc = self.database.get_acc(session, self.acc_name_1)
-        self.assertEqual(acc.balance, 100)
-        self.database.close(session)
+        with self.database.get_session() as session:
+            self.database.add_acc(session, self.acc_name_1, 100)
+            self.database.commit(session)
+            acc = self.database.get_acc(session, self.acc_name_1)
+            self.assertEqual(acc.balance, 100)
 
     def test_add_acc_second(self):
         """
         tests if a second account can be set by checking table
         columns of the first and the second entry
         """
-        session = self.database.get_session()
-        self.database.add_acc(session, self.acc_name_1, 100)
-        self.database.add_acc(session, self.acc_name_2, 200)
-        self.database.commit(session)
-        acc_user2 = self.database.get_acc(session, self.acc_name_2)
-        acc_user1 = self.database.get_acc(session, self.acc_name_1)
-        self.assertEqual(acc_user2.balance, 200)
-        self.assertEqual(acc_user1.balance, 100)
-        self.database.close(session)
+        with self.database.get_session() as session:
+            self.database.add_acc(session, self.acc_name_1, 100)
+            self.database.add_acc(session, self.acc_name_2, 200)
+            self.database.commit(session)
+            acc_user2 = self.database.get_acc(session, self.acc_name_2)
+            acc_user1 = self.database.get_acc(session, self.acc_name_1)
+            self.assertEqual(acc_user2.balance, 200)
+            self.assertEqual(acc_user1.balance, 100)
 
     def test_get_acc_none(self):
         """
         tests if get acc returns None if the
         specified account does not exist
         """
-        session = self.database.get_session()
-        acc = self.database.get_acc(session, self.acc_name_1)
-        self.assertEqual(acc, None)
-        self.database.close(session)
-
-    # def test_get_transactions_none(self):
-    #    """
-    #    tests if get transactions returns an empty list [] if the
-    #    specified account does have no transactions
-    #    """
-    #    self.database.add_acc("Daniel", 100)
-    #    transactions = self.database.get_all_transaction("Daniel")
-    #    self.assertEqual(transactions, [])
-
-    #def test_add_transactions_first(self):
-    #    """
-    #    tests if a second transaction can be set by checking table
-    #    columns of the first entry
-    #    """
-    #    session = self.database.get_session()
-    #    self.database.add_acc(session, self.acc_name_1, 100)
-    #    self.database.add_transaction(session, self.acc_name_1, 100, 50, "New Car")
-    #    self.database.commit(session)
-    #    acc_user1 = self.database.get_acc(session, self.acc_name_1)
-    #    self.assertEqual(acc_user1.transactions[0].amount, 100)
-    #    self.assertEqual(acc_user1.transactions[0].new_balance, 50)
-    #    self.assertEqual(acc_user1.transactions[0].description, "New Car")
-    #    self.database.close(session)
-
-    #def test_add_transactions_second(self):
-    #    """
-    #    tests if a second transaction can be set by checking table
-    #    columns of the first and the second entry
-    #    """
-    #    session = self.database.get_session()
-    #    self.database.add_acc(session, self.acc_name_1, 100)
-    #    self.database.add_transaction(session, self.acc_name_1, 100, 50, "New Car")
-    #    self.database.add_transaction(session, self.acc_name_1, 200, 100, "New Laptop")
-    #    self.database.commit(session)
-    #    acc_user1 = self.database.get_acc(session, self.acc_name_1)
-    #    self.assertEqual(acc_user1.transactions[0].amount, 100)
-    #    self.assertEqual(acc_user1.transactions[0].new_balance, 50)
-    #    self.assertEqual(acc_user1.transactions[0].description, "New Car")
-    #    self.assertEqual(acc_user1.transactions[1].amount, 200)
-    #    self.assertEqual(acc_user1.transactions[1].new_balance, 100)
-    #    self.assertEqual(acc_user1.transactions[1].description, "New Laptop")
-    #    self.database.close(session)
+        with self.database.get_session() as session:
+            acc = self.database.get_acc(session, self.acc_name_1)
+            self.assertEqual(acc, None)
 
     def tearDown(self):
         self.database._clear_all_tables()
--- PyMoneyOrga/test/unit/test_services_account.py
+++ PyMoneyOrga/test/unit/test_services_account.py
@@ -2,6 +2,7 @@ import unittest
 from PyMoneyOrga.service_layer import services_account
 from PyMoneyOrga.database.database_sqlite import DatabaseSqlite
 
+
 class TestServicesAccount(unittest.TestCase):
     def setUp(self):
         self.database = DatabaseSqlite(url="sqlite:///pymoneyorga.sqlite.test")
@@ -10,46 +11,47 @@ class TestServicesAccount(unittest.TestCase):
 
     def test_add_acc(self):
         services_account.add_acc(self.database, self.acc_name_1, 100)
-        session = self.database.get_session()
-        acc = self.database.get_acc(session, self.acc_name_1)
-        self.assertEqual(acc.acc_name, self.acc_name_1)
-        self.assertEqual(acc.balance, 100)
+        with self.database.get_session() as session:
+            acc = self.database.get_acc(session, self.acc_name_1)
+            self.assertEqual(acc.acc_name, self.acc_name_1)
+            self.assertEqual(acc.balance, 100)
 
     def test_add_income_updates_balance(self):
         services_account.add_acc(self.database, self.acc_name_1, 100)
         services_account.add_income(self.database, self.acc_name_1, 100)
-        session = self.database.get_session()
-        acc = self.database.get_acc(session, self.acc_name_1)
-        self.assertEqual(acc.balance, 200)
+        with self.database.get_session() as session:
+            acc = self.database.get_acc(session, self.acc_name_1)
+            self.assertEqual(acc.balance, 200)
 
     def test_add_income_adds_transaction(self):
         services_account.add_acc(self.database, self.acc_name_1, 100)
         services_account.add_income(self.database, self.acc_name_1, 100)
-        session = self.database.get_session()
-        acc = self.database.get_acc(session, self.acc_name_1)
-        self.assertEqual(acc.transactions[0].new_balance, 200)
-        self.assertEqual(acc.transactions[0].amount, 100)
-        self.assertEqual(acc.transactions[0].description, "income")
+        with self.database.get_session() as session:
+            acc = self.database.get_acc(session, self.acc_name_1)
+            self.assertEqual(acc.transactions[0].new_balance, 200)
+            self.assertEqual(acc.transactions[0].amount, 100)
+            self.assertEqual(acc.transactions[0].description, "income")
 
     def test_add_expense_updates_balance(self):
         services_account.add_acc(self.database, self.acc_name_1, 200)
         services_account.add_expense(self.database, self.acc_name_1, 50)
-        session = self.database.get_session()
-        acc = self.database.get_acc(session, self.acc_name_1)
-        self.assertEqual(acc.balance, 150)
+        with self.database.get_session() as session:
+            acc = self.database.get_acc(session, self.acc_name_1)
+            self.assertEqual(acc.balance, 150)
 
     def test_add_expense_adds_transaction(self):
         services_account.add_acc(self.database, self.acc_name_1, 200)
         services_account.add_expense(self.database, self.acc_name_1, 50)
-        session = self.database.get_session()
-        acc = self.database.get_acc(session, self.acc_name_1)
-        self.assertEqual(acc.transactions[0].new_balance, 150)
-        self.assertEqual(acc.transactions[0].amount, -50)
-        self.assertEqual(acc.transactions[0].description, "expense")
+        with self.database.get_session() as session:
+            acc = self.database.get_acc(session, self.acc_name_1)
+            self.assertEqual(acc.transactions[0].new_balance, 150)
+            self.assertEqual(acc.transactions[0].amount, -50)
+            self.assertEqual(acc.transactions[0].description, "expense")
 
     def tearDown(self):
         self.database._clear_all_tables()
         return super().tearDown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
