f4d99901b6372ec5721a6af8142c4d15f6437112
CRaNkXD <crank.xd@outlook.com>
2020-08-19 16:28:26 +0200

    Added class that to overwrite the displayText method in the QStyledItemDelegate class. Alows custom float formating.

---
+++ PyMoneyOrga/PyMoneyOrga/gui/float_delegate.py
@@ -0,0 +1,13 @@
+from PySide2 import QtWidgets, QtCore
+
+class FloatDelegate(QtWidgets.QStyledItemDelegate):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def displayText(self, value, locale):
+        try:
+            number = float(value)
+        except ValueError:
+            return super(FloatDelegate, self).displayText(value, locale)
+        else:
+            return f"{number:.2f}".replace('.', ',')
