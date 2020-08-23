b128ce8fd3d6703d90bb1587d550f42935910e8e
CRaNkXD <crank.xd@outlook.com>
2020-08-23 14:10:54 +0200

    added description and the possible pyside bug description

--- PyMoneyOrga/PyMoneyOrga/gui/float_delegate.py
+++ PyMoneyOrga/PyMoneyOrga/gui/float_delegate.py
@@ -2,6 +2,11 @@ from PySide2 import QtWidgets, QtCore
 
 
 class FloatDelegate(QtWidgets.QStyledItemDelegate):
+    """
+    ItemDelegate class for float values.
+    Can be used to specify the decimal precision shown in a table.
+    """
+
     def __init__(self, parent=None):
         super().__init__(parent)
 
@@ -12,3 +17,9 @@ class FloatDelegate(QtWidgets.QStyledItemDelegate):
             return super(FloatDelegate, self).displayText(value, locale)
         else:
             return f"{number:.2f}".replace(".", ",")
+
+        # There seems to be a bug in Pyside. If the parameter f or prec are used
+        # the toString methods always uses scientific notation.
+        # The precision is not applied to the decimals but to the whole number.
+
+        # return locale.toString(number, f="f", prec=2)
